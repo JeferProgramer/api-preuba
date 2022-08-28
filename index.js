02d6b58d9897a7f5dcdb36c191ee0ce9ea5c268e
JeferProgramer <jefer.talento.11@gmail.com>
2022-08-28 07:11:58 -0500

    vamos deploy

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 // Syncing all the models at once.
 //el force en true para que se me borre y cree la db
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT  || 3001,"0.0.0.0", () => {
     console.log('%s listening at 3001'); 
   });
 });
