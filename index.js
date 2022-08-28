cc6d8c1cae553458b8a7005963829e4567a2a1bb
JeferProgramer <jefer.talento.11@gmail.com>
2022-08-28 06:57:08 -0500

    tratando de arreglar el deploy de back

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 // Syncing all the models at once.
 //el force en true para que se me borre y cree la db
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT || 3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); 
   });
 });
